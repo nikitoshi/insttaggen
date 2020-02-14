8b5d8633b4582ae49815e1f61a21b60c41d02e4e
nikit0shi_ <nikitfree@gmail.com>
2020-02-15 00:01:14 +0300

    /.

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import taggen
+
+from telebot import apihelper
+
+
+# apihelper.proxy
+
+bot = telebot.TeleBot('1016000111:AAE51h1hhfe8PvRNO4hxjx5EOPCCyogrPJQ')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+bot.polling()
