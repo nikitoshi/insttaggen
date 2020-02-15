687c4e44a5bf4d2bc5d84a335ea507d4e0084342
nikitoshi <nikitfree@gmail.com>
2020-02-15 19:25:47 +0300

    chng PROXY

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import telebot
 from telebot import apihelper
 
 
-apihelper.proxy = {'http':'http://81.198.119.241:61418'}
+apihelper.proxy = {'http':'http://85.187.17.39:53281'}
 
 bot = telebot.TeleBot('1016000111:AAE51h1hhfe8PvRNO4hxjx5EOPCCyogrPJQ')
 
