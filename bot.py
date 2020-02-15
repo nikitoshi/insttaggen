37257d6d424796b76a83dde718b0b33acd5463cb
nikitoshi <nikitfree@gmail.com>
2020-02-16 01:18:11 +0300

    +

--- bot.py
+++ bot.py
@@ -27,11 +27,8 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-     This handler will be called when user sends `/start` or `/help` command
-     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+async def start_message(message):
+   bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 
 if __name__ == '__main__':
