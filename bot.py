9830493c0fa3f6bc79ab6668da95dc9939018336
nikitoshi <nikitfree@gmail.com>
2020-02-16 00:36:15 +0300

    dotenv_path

--- bot.py
+++ bot.py
@@ -2,6 +2,11 @@
 # -*- coding: utf8 -*-
 import logging
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 import aiohttp
 
@@ -11,6 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+
 PROXY_AUTH = aiohttp.BasicAuth(
     login=os.getenv("TELEGRAM_PROXY_LOGIN"),
     password=os.getenv("TELEGRAM_PROXY_PASSWORD")
@@ -19,9 +25,14 @@ PROXY_AUTH = aiohttp.BasicAuth(
 bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+     This handler will be called when user sends `/start` or `/help` command
+     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
