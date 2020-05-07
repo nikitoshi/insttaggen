1918b7832c79a801bdb57e4276589bd07cafe5d4
nikit0shi_ <nikitfree@gmail.com>
2020-05-07 20:32:56 +0300

    PROXY disable

--- bot.py
+++ bot.py
@@ -30,14 +30,15 @@ tag_list = wks.get_all_values()
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+# PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+#
+# PROXY_AUTH = aiohttp.BasicAuth(
+#     login=os.getenv("TELEGRAM_PROXY_LOGIN"),
+#     password=os.getenv("TELEGRAM_PROXY_PASSWORD")
+# )
 
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
-
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+# bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
